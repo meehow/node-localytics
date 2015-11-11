37952e07607bed6491323e52431c4104061d41dd
Michał Adamski <michal@ert.pl>
2015-11-11 20:00:51 +0100

    example simplified

--- example.js
+++ example.js
@@ -4,7 +4,4 @@ var localytics = require('localytics')();
 
 localytics.setProfile('552bc23ef2eeb6ff036a0f45', {
   fullyRegistered: 1,
-}).then(console.log).catch(function(e) {
-  console.log('code', e.code);
-  console.log(e.stack);
-});
+}).then(console.log).catch(console.log);
